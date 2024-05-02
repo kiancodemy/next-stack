b480b4e360c0c8768b91df3b1ec586cb3c684337
kian kheirani <kiankheirani@gmail.com>
2024-05-02 12:59:46 +0430

    'add-products-section'

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
+dotenv.config();
 const app = express();
 app.use(cors());
+
 app.use(express.json());
-app.get("/api", async (req, res) => {
-  res.status(200).json({ data: "kire" });
+app.get("/api", (req, res) => {
+  res.json({ data: "kire" });
 });
 app.listen(process.env.PORT, () => {
   console.log("app is connected");
