cd9b3d7eed66f89cbafee2014aa2a484090ea9a0
kian kheirani <kiankheirani@gmail.com>
2024-05-02 08:53:23 +0430

    'add'

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from "express";
+import cors from "cors";
 const app = express();
+app.use(cors());
 app.use(express.json());
-
+app.get("/api", async (req, res) => {
+  res.status(200).json({ data: "kire" });
+});
 app.listen(process.env.PORT, () => {
   console.log("app is connected");
 });
